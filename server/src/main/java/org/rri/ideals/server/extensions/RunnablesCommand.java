3609fe8e1216f23e5e6170d14cf2147a2cfa50a1
Emmanuel Lebeaupin <9947807+elebeaup@users.noreply.github.com>
2025-03-19 16:20:06 +0100

    Fix missing runnable test methods

--- server/src/main/java/org/rri/ideals/server/extensions/RunnablesCommand.java
+++ server/src/main/java/org/rri/ideals/server/extensions/RunnablesCommand.java
@@ -2,8 +2,10 @@ package org.rri.ideals.server.extensions;
 
 import com.intellij.codeInsight.daemon.impl.DaemonCodeAnalyzerImpl;
 import com.intellij.execution.ExecutionException;
+import com.intellij.execution.PsiLocation;
 import com.intellij.execution.actions.BaseRunConfigurationAction;
 import com.intellij.execution.actions.ConfigurationContext;
+import com.intellij.execution.actions.ConfigurationFromContext;
 import com.intellij.execution.configurations.JavaCommandLine;
 import com.intellij.execution.lineMarker.ExecutorAction;
 import com.intellij.execution.lineMarker.LineMarkerActionWrapper;
@@ -17,6 +19,7 @@ import org.rri.ideals.server.commands.ExecutorContext;
 import org.rri.ideals.server.commands.LspCommand;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Supplier;
@@ -26,59 +29,63 @@ import static org.rri.ideals.server.util.MiscUtil.distinctByKey;
 
 public class RunnablesCommand extends LspCommand<List<Runnable>> {
 
-    private static final Logger LOG = Logger.getInstance(RunnablesCommand.class);
+  private static final Logger LOG = Logger.getInstance(RunnablesCommand.class);
 
-    @Override
-    protected @NotNull Supplier<@NotNull String> getMessageSupplier() {
-        return () -> "experimental/runnables call";
-    }
+  @Override
+  protected @NotNull Supplier<@NotNull String> getMessageSupplier() {
+    return () -> "experimental/runnables call";
+  }
 
-    @Override
-    protected boolean isCancellable() {
-        return false;
-    }
+  @Override
+  protected boolean isCancellable() {
+    return false;
+  }
 
-    @Override
-    protected List<Runnable> execute(@NotNull ExecutorContext ctx) {
-        final var project = ctx.getEditor().getProject();
+  @Override
+  protected List<Runnable> execute(@NotNull ExecutorContext ctx) {
+    final var project = ctx.getEditor().getProject();
 
-        return DaemonCodeAnalyzerImpl.getLineMarkers(ctx.getEditor().getDocument(), project)
-                .stream()
-                .flatMap(lineMarkerInfo -> {
-                    var gutter = lineMarkerInfo.createGutterRenderer();
+    return DaemonCodeAnalyzerImpl.getLineMarkers(ctx.getEditor().getDocument(), project)
+        .stream()
+        .flatMap(lineMarkerInfo -> {
+          var gutter = lineMarkerInfo.createGutterRenderer();
 
-                    if (gutter.getPopupMenuActions() != null) {
-                        return Arrays.stream(((DefaultActionGroup) gutter.getPopupMenuActions()).getChildActionsOrStubs()).filter(anAction -> "Run context configuration".equals(anAction.getTemplateText())).filter(anAction -> anAction instanceof LineMarkerActionWrapper).map(anAction -> {
-                            var lineMarkerActionWrapper = (LineMarkerActionWrapper) anAction;
-                            var dataContext = SimpleDataContext.builder().add(CommonDataKeys.PROJECT, project).add(CommonDataKeys.PSI_ELEMENT, lineMarkerInfo.getElement()).build();
-                            var executor = ((ExecutorAction) lineMarkerActionWrapper.getDelegate()).getExecutor();
-                            var configurationContext = ConfigurationContext.getFromContext(dataContext, "");
-                            var runnerAndConfigurationSettings = configurationContext.findExisting();
+          if (gutter.getPopupMenuActions() != null && lineMarkerInfo.getElement() != null) {
+            return Arrays.stream(((DefaultActionGroup) gutter.getPopupMenuActions()).getChildActionsOrStubs()).filter(anAction -> "Run context configuration".equals(anAction.getTemplateText())).filter(anAction -> anAction instanceof LineMarkerActionWrapper).map(anAction -> {
+              var lineMarkerActionWrapper = (LineMarkerActionWrapper) anAction;
+              var dataContext = SimpleDataContext.builder().add(CommonDataKeys.PROJECT, project)
+                  .add(CommonDataKeys.PSI_ELEMENT, lineMarkerInfo.getElement())
+                  .add(PsiLocation.DATA_KEY, new PsiLocation<>(lineMarkerInfo.getElement()))
+                  .build();
+              var executor = ((ExecutorAction) lineMarkerActionWrapper.getDelegate()).getExecutor();
+              var configurationContext = ConfigurationContext.getFromContext(dataContext, "");
+              var contexts = configurationContext.getConfigurationsFromContext();
+              if (contexts != null && !contexts.isEmpty()) {
+                var min = Collections.min(contexts, ConfigurationFromContext.COMPARATOR);
+                var runnerAndConfigurationSettings = min.getConfigurationSettings();
+                var actionName = executor.getActionName() + " '" + BaseRunConfigurationAction.suggestRunActionName(runnerAndConfigurationSettings.getConfiguration()) + "'";
 
-                            if (runnerAndConfigurationSettings != null) {
-                                var actionName = executor.getActionName() + " '" + BaseRunConfigurationAction.suggestRunActionName(runnerAndConfigurationSettings.getConfiguration()) + "'";
+                try {
+                  var environment = ExecutionEnvironmentBuilder.create(executor, runnerAndConfigurationSettings).build();
+                  var currentState = environment.getState();
+                  var commandLine = ((JavaCommandLine) currentState).getJavaParameters().toCommandLine();
+                  return new Runnable(actionName, new Runnable.Arguments(
+                      commandLine.getWorkingDirectory().toString(),
+                      commandLine.getExePath(),
+                      commandLine.getParametersList().getList()
+                  ));
+                } catch (ExecutionException e) {
+                  LOG.error("Unable to create an execution environment", e);
+                }
+              }
 
-                                try {
-                                    var environment = ExecutionEnvironmentBuilder.create(executor, runnerAndConfigurationSettings).build();
-                                    var currentState = environment.getState();
-                                    var commandLine = ((JavaCommandLine) currentState).getJavaParameters().toCommandLine();
-                                    return new Runnable(actionName, new Runnable.Arguments(
-                                            commandLine.getWorkingDirectory().toString(),
-                                            commandLine.getExePath(),
-                                            commandLine.getParametersList().getList()
-                                    ));
-                                } catch (ExecutionException e) {
-                                    LOG.error("Unable to create an execution environment", e);
-                                }
-                            }
+              return null;
+            }).filter(Objects::nonNull);
+          }
 
-                            return null;
-                        }).filter(Objects::nonNull);
-                    }
-
-                    return Stream.of();
-                })
+          return Stream.of();
+        })
                 .filter(distinctByKey(Runnable::label))
-                .toList();
-    }
+        .toList();
+  }
 }
